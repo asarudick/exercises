ec5b6484597a6708c27e2f96ef35c36a4802cae2
Asa Rudick <Shabonkerz@gmail.com>
2015-04-06 10:19:19 -0500

    add details python exercise

---
+++ python/details.py
@@ -0,0 +1,31 @@
+import sys
+
+test_cases = open(sys.argv[1],'r')
+
+for line in test_cases:
+
+	# Strip new line at end
+	line = line.strip()
+
+	# Load details into a rectangular matrix
+	matrix = line.split(',')
+
+	replacements = 0
+	while True:
+
+		fin = False
+
+		for col in xrange(len(matrix)):
+			row = matrix[col]
+			if '.Y' in row and not 'XY' in row:
+				matrix[col] = row.replace('.Y', 'Y')
+			else:
+				fin = True
+				break
+
+		if fin:
+			break
+		
+		replacements += 1
+
+	print(replacements)
