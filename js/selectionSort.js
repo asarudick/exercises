78ca2a34d5d815ca72bbd6d4f4c6ef17697ab463
Shabonkerz <Shabonkerz@gmail.com>
2016-04-04 14:59:52 -0500

    add selection sort exercise

---
+++ js/selectionSort.js
@@ -0,0 +1,62 @@
+function defaultCmp (a, b) {
+    const result = a - b;
+
+    if (result > 0)
+    {
+        return 1;
+    }
+
+    if (result < 0)
+    {
+        return -1;
+    }
+
+    return 0;
+};
+
+/**
+ * Copies the given array, and returns it in sorted fashion using selection sort.
+ *
+ * Process for selection sort: The array is partitioned into 2 subarrays,
+ * where initially the "sorted" subarray is comprised of zero elements, and the
+ * unsorted subarray is the array given. The pointer i always points to the
+ * first element in the unsorted subarray, and j scans the rest of the unsorted
+ * to find the minimum element to swap with i. If none less than the element i
+ * is found, no swap occurs. After this process, the element at index i becomes
+ * the last element of the sorted subarray. This iterative process continues
+ * until there is only one element left in the unsorted subarray, in which case
+ * the remaining element is the minimum(no other element to compare against),
+ * and is thusly deemed the end of the sorted subarray.
+ *
+ * @param  {Array}      arr     An array comprised of comparable elements.
+ * @param  {Function}   cmp     The comparator to perform the comparison between elements in the array.
+ * @return {Array}              The array in sorted fashion.
+ */
+export default function selectionSort (arr, cmp) {
+
+    const result = Array.prototype.slice.call(arr);
+    const comparator = cmp || defaultCmp;
+    const length = result.length;
+
+    for (let i = 0; i < length - 1; i++)
+    {
+        let minimumIndex = i;
+
+        for (let j = i + 1; j < length; j++)
+        {
+            if (comparator(result[j], result[minimumIndex]) === -1) {
+                minimumIndex = j;
+            }
+        }
+
+        // Performing a swap at identical indexes leads to no change.
+        if (minimumIndex !== i)
+        {
+            const swap = result[i];
+            result[i] = result[minimumIndex];
+            result[minimumIndex] = swap;
+        }
+    }
+
+    return result;
+}
---
+++ js/tests/selectionSort.js
@@ -0,0 +1,46 @@
+import selectionSort from '../selectionSort';
+import assert from 'assert';
+
+describe('selectionSort', () => {
+    it('should sort [] into []', () => {
+        const arr = [ ];
+        const result = selectionSort(arr);
+        assert.deepEqual(result, [ ]);
+    });
+    it('should sort [1] into [1]', () => {
+        const arr = [ 1 ];
+        const result = selectionSort(arr);
+        assert.deepEqual(result, [ 1 ]);
+    });
+    it('should sort [2,1] into [1,2]', () => {
+        const arr = [ 2, 1 ];
+        const result = selectionSort(arr);
+        assert.deepEqual(result, [ 1, 2 ]);
+    });
+    it('should sort [3,2,1] into [1,2,3]', () => {
+        const arr = [ 3, 2, 1 ];
+        const result = selectionSort(arr);
+        assert.deepEqual(result, [ 1, 2, 3 ]);
+    });
+    it('should sort [15,10,5] into [5,10,15]', () => {
+        const arr = [ 15, 10, 5 ];
+        const result = selectionSort(arr);
+        assert.deepEqual(result, [ 5, 10, 15 ]);
+    });
+    it('should use user-defined comparator and sort [ {i: 15}, {i: 10}, {i:5} ] into [ {i: 5}, {i: 10}, {i:15} ]', () => {
+        const arr = [ {i: 15}, {i: 10}, {i: 5} ];
+        const comparator = (a, b) => {
+            if (a.i < b.i)
+            {
+                return -1;
+            }
+            if (a.i > b.i)
+            {
+                return 1;
+            }
+            return 0;
+        };
+        const result = selectionSort(arr, comparator);
+        assert.deepEqual(result, [ {i: 5}, {i: 10}, {i: 15} ]);
+    });
+});
