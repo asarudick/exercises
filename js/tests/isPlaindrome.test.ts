db666cccfa97570496b60b284ac997b446ccb592
asarudick <asa.rudick@gmail.com>
2024-08-16 01:36:02 -0700

    add isPlaindrome exercise tests

--- js/tests/isPlaindrome.test.ts
+++ js/tests/isPlaindrome.test.ts
@@ -1,12 +1,40 @@
 import isPalindrome from '../isPalindrome';
 
 describe('isPalindrome', () => {
-	test('should function', () => {
-		const result = isPalindrome("PAYPALISHIRING", 3);
-        expect(result).toEqual("PAHNAPLSIIGYIR");
+	test('1', () => {
+		const result = isPalindrome(1);
+        expect(result).toEqual(true);
 	});
-    test('ABCD -> ACBD', () => {
-        const result = isPalindrome("ABCD", 2);
-        expect(result).toEqual("ACBD");
+	test('11', () => {
+		const result = isPalindrome(11);
+        expect(result).toEqual(true);
+	});
+	test('12', () => {
+		const result = isPalindrome(12);
+        expect(result).toEqual(false);
+	});
+	test('121', () => {
+		const result = isPalindrome(121);
+        expect(result).toEqual(true);
+	});
+	test('1221', () => {
+		const result = isPalindrome(1221);
+        expect(result).toEqual(true);
+	});
+    test('-1', () => {
+		const result = isPalindrome(-1);
+        expect(result).toEqual(false);
+    });
+    test('-12', () => {
+		const result = isPalindrome(-12);
+        expect(result).toEqual(false);
+    });
+    test('-121', () => {
+		const result = isPalindrome(-121);
+        expect(result).toEqual(false);
+    });
+    test('123454321', () => {
+		const result = isPalindrome(123454321);
+        expect(result).toEqual(true);
     });
 });
