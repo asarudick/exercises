ff87a8e304deb454ab4927989f1fe70bd7c55552
asarudick <asa.rudick@gmail.com>
2024-08-20 04:01:42 -0700

    touchup

--- js/tests/longestCommonPrefix.test.ts
+++ js/tests/longestCommonPrefix.test.ts
@@ -3,6 +3,6 @@ import longestCommonPrefix from '../longestCommonPrefix';
 describe('longestCommonPrefix', () => {
 	test('["flower","flow","flight"] -> fl', () => {
 		const result = longestCommonPrefix([ "flower", "flow", "flight" ]);
-        expect(result).toEqual(1);
+        expect(result).toEqual('fl');
 	});
 });
