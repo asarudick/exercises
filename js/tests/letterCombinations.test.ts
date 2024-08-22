f2c1e2c8f42a47d2c07964567b2bcc43452897bb
asarudick <asa.rudick@gmail.com>
2024-08-22 15:33:59 -0700

    update letter combinations test

--- js/tests/letterCombinations.test.ts
+++ js/tests/letterCombinations.test.ts
@@ -1,6 +1,14 @@
 import letterCombinations from '../letterCombinations';
 
 describe('letterCombinations', () => {
+	test('"" -> []', () => {
+		const result = letterCombinations("");
+        expect(result).toEqual([]);
+	});
+	test('"2" -> ["a", "b", "c"]', () => {
+		const result = letterCombinations("2");
+        expect(result).toEqual(["a", "b", "c"]);
+	});
 	test('"23" -> ["ad","ae","af","bd","be","bf","cd","ce","cf"]', () => {
 		const result = letterCombinations("23");
         expect(result).toEqual(["ad","ae","af","bd","be","bf","cd","ce","cf"]);
