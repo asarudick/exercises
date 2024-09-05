e55d943695256dc2bd2b44fd80964aec2178e6b8
asarudick <asa.rudick@gmail.com>
2024-09-05 13:18:36 -0700

    ♻️ (combinationSumII.ts): remove unused combinationSumRecurse function to clean up code

--- js/combinationSumII.ts
+++ js/combinationSumII.ts
@@ -1,34 +1,3 @@
-function combinationSumRecurse(
-  candidates: number[],
-  target: number,
-  current: number[],
-  results: number[][],
-  rem: number,
-  start: number = 0,
-  used: boolean[]
-) {
-  if (rem === 0) {
-    results.push(Array.from(current));
-    return;
-  }
-
-  if (rem < (candidates[start] as number)) {
-    return;
-  }
-
-  for (let i = start; i < candidates.length; i++) {
-    if (i > 0 && candidates[i] === candidates[i - 1] && used[i - 1] === false) {
-      continue;
-    }
-    used[i] = true;
-    current.push(candidates[i] as number);
-    rem -= candidates[i] as number;
-    combinationSumRecurse(candidates, target, current, results, rem, i, used);
-    used[i] = false;
-    rem += current.pop() as number;
-  }
-}
-
 export default function combinationSum2(
   candidates: number[],
   target: number
