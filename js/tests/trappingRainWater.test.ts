6fe17ec0efa0954bbbd31f27d463f91ed2ccfeff
asarudick <asa.rudick@gmail.com>
2024-09-05 14:38:58 -0700

    test: add unit tests for trappingRainWater function

---
+++ js/tests/trappingRainWater.test.ts
@@ -0,0 +1,8 @@
+import trappingRainWater from '../trappingRainWater';
+
+describe('trappingRainWater', () => {
+  it('should work correctly', () => {
+    // TODO: Add test cases
+    expect(trappingRainWater()).toBeUndefined();
+  });
+});
