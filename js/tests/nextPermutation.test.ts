6ec1d97f8eae86caf7a14f9b864d4866dd0fd601
asarudick <asa.rudick@gmail.com>
2024-09-06 10:14:09 -0700

    feat: Add test suite for nextPermutation function

---
+++ js/tests/nextPermutation.test.ts
@@ -0,0 +1,8 @@
+import nextPermutation from '../nextPermutation';
+
+describe('nextPermutation', () => {
+  it('should work correctly', () => {
+    // TODO: Add test cases
+    expect(nextPermutation()).toBeUndefined();
+  });
+});
