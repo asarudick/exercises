dea29ddb9390966e3572616edf9ba5474bd9a0f7
asarudick <asa.rudick@gmail.com>
2024-09-11 10:14:21 -0700

    ✨ (TreeNode.ts): add generic type support to TreeNode class ✨ (TreeNode.ts): add create function to build a tree from an array of values

--- js/lib/TreeNode.ts
+++ js/lib/TreeNode.ts
@@ -1,10 +1,33 @@
-export default class TreeNode {
-  val: number;
-  left: TreeNode | null;
-  right: TreeNode | null;
-  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-    this.val = val === undefined ? 0 : val;
+export default class TreeNode<T> {
+  val: T | null;
+  left: TreeNode<T> | null;
+  right: TreeNode<T> | null;
+  constructor(val?: T, left?: TreeNode<T> | null, right?: TreeNode<T> | null) {
+    this.val = val === undefined ? null : val;
     this.left = left === undefined ? null : left;
     this.right = right === undefined ? null : right;
   }
+}
+export function create<T>(
+  values: (T | null)[]
+): TreeNode<T> | null | undefined {
+  if (!values.length) return null;
+
+  const nodes = values.map((val) =>
+    val !== null ? new TreeNode<T>(val) : null
+  );
+  const root = nodes[0];
+
+  while (nodes.length) {
+    let node = nodes.shift();
+    if (!node) {
+      continue;
+    }
+    node.left = nodes.shift() as TreeNode<T> | null;
+    node.right = nodes.shift() as TreeNode<T> | null;
+    nodes.unshift(node.left);
+    nodes.unshift(node.right);
+  }
+
+  return root;
 }
